fda06d7b5cdc8445bb8ac02db975894a55d6a627
Neto Lourenço <otaviojnetoo@gmail.com>
2021-08-31 22:57:53 -0300

    link

--- src/mocks/DataMenuActions.js
+++ src/mocks/DataMenuActions.js
@@ -9,19 +9,19 @@ const MenuActions = [
     id: 3,
     name: 'Monte a sua',
     ref: 'monte-a-sua',
-    link: 'https://toro.fiat.com.br/monte.html',
+    link: '#',
   },
     {
     id: 2,
     name: 'Simule o financiamento',
     ref: 'simule-o-financiamento',
-    link: 'https://toro.fiat.com.br/monte.html',
+    link: '#',
   },
   {
     id: 4,
     name: 'Concessionária',
     ref: 'concessionaria',
-    link: 'https://www.fiat.com.br/concessionarias.html',
+    link: '#',
   },
   // {
   //   id: 5,
